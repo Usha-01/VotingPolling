c5d072df5d7319b4056ab22e9d61d0e1fe0b1bdc
Usha Kalathinathan <ushasasi7018@gmail.com>
2023-07-11 17:35:23 +0530

    DAY 4
    Page Navigation

---
+++ DAY 4/App.js
@@ -0,0 +1,59 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import LoginVoter from './Pages/LoginPage';
+import Select from './Pages/Select';
+import RegistrationForm from './Pages/RegistrationV';
+import AdminVoter from './Pages/Admin';
+
+import './App.css';
+import CandidateRegistrationForm from './Pages/RegistrationC';
+
+
+function App() {
+  
+  return (
+    <>
+  <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={'/'}>
+              Home page
+            </Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-in'}>
+                    User Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-up'}>
+                    Admin Login
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/" element={<Select />} />
+              <Route path="/sign-in" element={<LoginVoter />} />
+              <Route path="/sign-up" element={<AdminVoter/>} />
+              <Route path="/RegistrationV" element={<RegistrationForm/>}/>
+              <Route path="/RegistrationC" element={<CandidateRegistrationForm/>}/>
+       
+      </Routes>
+     
+            
+          </div>
+        </div>
+      </div>
+    </Router>
+   </>
+  );
+}
+
+export default App;
