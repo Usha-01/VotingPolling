66a48f360d57ecc9b65c0962ee5b7a6c9e02b311
Usha Kalathinathan <ushasasi7018@gmail.com>
2023-07-12 17:59:07 +0530

    DAY 6
    Front End Development

---
+++ DAY 6/Pages/Select.js
@@ -0,0 +1,25 @@
+import React from "react";
+// import admin from './Image/imgvote.jpeg
+import "./home.css"
+
+export default function Select(){
+    return(
+        <div id="home1">
+            <br></br>
+            <div className="v">
+                <p>
+                *Go early and cast your vote.<br></br>
+                 *Your vote can change the way things are.<br></br>
+                  *Your vote can be the next big change.<br></br>
+                   *It's your right; don't let anyone else cast it.
+                </p>
+            </div>
+            <div className="ho">
+            <h3><i>Here you go with EVoting!!!</i></h3>
+            </div>
+            </div> 
+        
+          
+    )
+}
+
